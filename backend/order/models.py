91c2c5150a3e14e5594786e90cb6d39a999901f1
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2025-08-21 21:18:36 +0600

    B:: order status

--- backend/order/models.py
+++ backend/order/models.py
@@ -14,14 +14,14 @@ class Order(models.Model):
         max_length=20, 
         choices=[
             ("paid", "paid"),
-            ("pay_first", "pay_first"),
+            ("payment", "Payment Pending"),
             ("pending", "Pending"),
             ("processing", "Processing"),
             ("shipped", "shipped"),
             ("delivered", "delivered"),
             ("cancelled", "Cancelled"),
         ], 
-        default="pay_first"
+        default="payment"
     )
     total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
