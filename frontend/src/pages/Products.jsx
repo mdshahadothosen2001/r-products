7a44557b5c11bcb2adf087e92fa243fbba5e4bde
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2025-08-21 11:31:25 +0600

    F:: products page active

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,5 +1,10 @@
+import Banner from "../components/Banner";
+import NavBar from "../components/NavBar";
+import WelcomeNavBar from "../components/WelcomeNavBar";
 import Home from "./Home";
 
 export default function Products() {
-  return <Home />; // Reuse Home for product listing for now
+  return (
+    <h1>Hello</h1>
+  );
 }
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
-// import Products from "../pages/Products";
+import Products from "../pages/Products";
 // import ProductDetails from "../pages/ProductDetails";
 // import Cart from "../pages/Cart";
 // import Checkout from "../pages/Checkout";
@@ -17,8 +17,8 @@ export default function AppRoutes() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        {/* <Route path="/products" element={<Products />} />
-        <Route path="/products/:id" element={<ProductDetails />} />
+        <Route path="/products" element={<Products />} />
+        {/* <Route path="/products/:id" element={<ProductDetails />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/orders" element={<OrderHistory />} />
