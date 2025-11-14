0516ef5a67dff4efdfbc919b4181a99abdaf4813
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2025-11-14 10:48:45 +0600

    Update run-commands.go to include loading of banner fixtures and comment out old product fixtures

--- backend/run-commands.go
+++ backend/run-commands.go
@@ -1,4 +1,6 @@
 python manage.py loaddata category/fixtures/categories.json
 python manage.py loaddata category/fixtures/subcategories.json
 python manage.py loaddata product/fixtures/products.json
+// python manage.py loaddata product/fixtures/product-old.json
+python manage.py loaddata banner/fixtures/banners.json
 // python manage.py seed coupon --number=20
