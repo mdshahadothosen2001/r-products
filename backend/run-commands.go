1b12a2f1431600c372e1584019ebf684107b11f1
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2025-11-14 08:47:02 +0600

    Add subcategory fixtures and update category loading commands

--- backend/run-commands.go
+++ backend/run-commands.go
@@ -1,3 +1,4 @@
-python manage.py loaddata product/fixtures/categories.jso
-python manage.py loaddata product/fixtures/products.jso
-python manage.py seed coupon --number=20
+python manage.py loaddata category/fixtures/categories.json
+python manage.py loaddata category/fixtures/subcategories.json
+python manage.py loaddata product/fixtures/products.json
+// python manage.py seed coupon --number=20
